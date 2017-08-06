eadd4d09d4fd738d030ff60982670b03acbf07f9
shogo4405 <shogo4405@gmail.com>
2017-08-06 22:23:31 +0900

    Change ConsoleAppender log format

--- Sources/ConsoleAppender.swift
+++ Sources/ConsoleAppender.swift
@@ -2,9 +2,9 @@ import Foundation
 
 public class ConsoleAppender: LogboardAppender {
     public func append(_ logboard:Logboard, level: Logboard.Level, message:String, file:StaticString, function:StaticString, line:Int) {
-        print("[", level, "] [", logboard.identifier, "] [", line, "] > ", message)
+        print("[\(level)]", "[\(logboard.identifier)]", "[\(line)]", function, ">", message)
     }
     public func append(_ logboard:Logboard, level: Logboard.Level, format:String, arguments:CVarArg, file:StaticString, function:StaticString, line:Int) {
-        print("[", level, "] [", logboard.identifier, "] [", line, "] > ", String(format: format, arguments))
+        print("[\(level)]", "[\(logboard.identifier)]", "[\(line)]", function, ">", String(format: format, arguments))
     }
 }
