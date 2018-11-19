a91583653cc394f6bd2d8003ae4ea546064a50c6
shogo4405 <shogo4405@gmail.com>
2018-11-20 03:10:57 +0900

    fix ignore log level..

--- Sources/Logboard.swift
+++ Sources/Logboard.swift
@@ -73,42 +73,52 @@ public class Logboard {
     }
 
     public func trace(_ message: String, file: StaticString = #file, function: StaticString = #function, line: Int = #line) {
+        guard isEnabledFor(level: .trace) else { return }
         appender.append(self, level: .trace, message: message, file: file, function: function, line: line)
     }
 
     public func trace(format: String, arguments: CVarArg, file: StaticString = #file, function: StaticString = #function, line: Int = #line) {
+        guard isEnabledFor(level: .trace) else { return }
         appender.append(self, level: .trace, format: format, arguments: arguments, file: file, function: function, line: line)
     }
 
     public func debug(_ message: String, file: StaticString = #file, function: StaticString = #function, line: Int = #line) {
+        guard isEnabledFor(level: .debug) else { return }
         appender.append(self, level: .debug, message: message, file: file, function: function, line: line)
     }
 
     public func debug(format: String, arguments: CVarArg, file: StaticString = #file, function: StaticString = #function, line: Int = #line) {
+        guard isEnabledFor(level: .debug) else { return }
         appender.append(self, level: .debug, format: format, arguments: arguments, file: file, function: function, line: line)
     }
 
     public func info(_ message: String, file: StaticString = #file, function: StaticString = #function, line: Int = #line) {
+        guard isEnabledFor(level: .info) else { return }
         appender.append(self, level: .info, message: message, file: file, function: function, line: line)
     }
 
     public func info(format: String, arguments: CVarArg, file: StaticString = #file, function: StaticString = #function, line: Int = #line) {
+        guard isEnabledFor(level: .info) else { return }
         appender.append(self, level: .info, format: format, arguments: arguments, file: file, function: function, line: line)
     }
 
     public func warn(_ message: String, file: StaticString = #file, function: StaticString = #function, line: Int = #line) {
+        guard isEnabledFor(level: .warn) else { return }
         appender.append(self, level: .warn, message: message, file: file, function: function, line: line)
     }
 
     public func warn(format: String, arguments: CVarArg, file: StaticString = #file, function: StaticString = #function, line: Int = #line) {
+        guard isEnabledFor(level: .warn) else { return }
         appender.append(self, level: .warn, format: format, arguments: arguments, file: file, function: function, line: line)
     }
 
     public func error(_ message: String, file: StaticString = #file, function: StaticString = #function, line: Int = #line) {
+        guard isEnabledFor(level: .error) else { return }
         appender.append(self, level: .error, message: message, file: file, function: function, line: line)
     }
 
     public func error(format: String, arguments: CVarArg, file: StaticString = #file, function: StaticString = #function, line: Int = #line) {
+        guard isEnabledFor(level: .error) else { return }
         appender.append(self, level: .error, format: format, arguments: arguments, file: file, function: function, line: line)
     }
 }
