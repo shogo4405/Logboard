1927ef86b6898564a0d57e530ca2c3ee30c0b2a2
shogo4405 <shogo4405@gmail.com>
2019-09-21 15:36:55 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,18 +6,12 @@ import PackageDescription
 let package = Package(
     name: "Logboard",
     products: [
-        .library(
-            name: "Logboard",
-            targets: ["Logboard"])
+        .library(name: "Logboard", targets: ["Logboard"])
     ],
     dependencies: [
     ],
     targets: [
-        .target(
-            name: "Logboard",
-            dependencies: []),
-        .testTarget(
-            name: "LogboardTests",
-            dependencies: ["Logboard"])
+        .target(name: "Logboard", dependencies: []),
+        .testTarget(name: "LogboardTests", dependencies: ["Logboard"])
     ]
 )
