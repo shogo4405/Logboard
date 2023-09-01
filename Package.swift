8c2159765abf295ca5e0c796db444cd401f9316d
shogo4405 <shogo4405@gmail.com>
2023-09-02 05:36:25 +0900

    Update documents.

--- Package.swift
+++ Package.swift
@@ -19,6 +19,12 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
-        .target(name: "Logboard", dependencies: []),
+        .target(
+             name: "Logboard",
+             dependencies: [],
+             exclude: [
+                 "Info.plist"
+             ]
+        )
     ]
 )
